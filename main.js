3cc84388260a5ed814b21f9861d698180606008d
James Clarke <jaclarke77@gmail.com>
2015-03-13 19:53:14 +0000

    Fix error in loading progress data

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ function initMap() {
 
   // init progress data
 
-  progressData = Tabletop.init({ key: '1HpgLgPfUj-JLnKWLNFkApnYufgLTWcR1y3cqJHytyj0', callback: updateMarker, simpleSheet: true, parseNumbers: true, prettyColumnNames: false })
+  progressData = Tabletop.init({ key: '1HpgLgPfUj-JLnKWLNFkApnYufgLTWcR1y3cqJHytyj0', callback: updateProgress, simpleSheet: true, parseNumbers: true, prettyColumnNames: false })
 
   
 }
